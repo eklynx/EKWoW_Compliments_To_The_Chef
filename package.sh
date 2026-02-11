7f8b1d55572d1530ffdf8aea57b8cd96bb7f776c
Edgars Klepers <3709300+eklynx@users.noreply.github.com>
2026-02-10 20:30:22 -0800

    adding packag.sh script and .gitignore

---
+++ package.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+cd ..
+zip EKWoW_Compliments_To_The_Chef/EKWoW_Compliments_To_The_Chef.zip EKWoW_Compliments_To_The_Chef/*.toc EKWoW_Compliments_To_The_Chef/*.lua EKWoW_Compliments_To_The_Chef/LICENSE
+cd EKWoW_Compliments_To_The_Chef || exit
